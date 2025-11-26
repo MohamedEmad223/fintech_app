541ec22dbfa4f8d48553c6aeda3abc64a91f55a8
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-11-26 18:20:43 +0200

    added private constructor

--- lib/core/theming/app_assets.dart
+++ lib/core/theming/app_assets.dart
@@ -1,4 +1,5 @@
-abstract class AppAssets {
+class AppAssets {
+  AppAssets._();
   static const String faliurArrowSvg = 'assets/svgs/faliur_arrow.svg';
   static const String successArrowSvg = 'assets/svgs/success_arrow.svg';
 }
