ecbd41729cee624b8856bc5206c2f2ed5ac72f6a
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-11-26 19:01:37 +0200

    - make my holding model & format api_services

---
+++ lib/features/portfolio/data/models/my_holding_model.dart
@@ -0,0 +1,21 @@
+class MyHoldingModel {
+  final String icon;
+  final String name;
+  final String symbol;
+  final String amount;
+  final String dollarValue;
+  final String portfolioPercentage;
+  final double priceChange;
+  final double percentageChange;
+
+  MyHoldingModel({
+    required this.icon,
+    required this.name,
+    required this.symbol,
+    required this.amount,
+    required this.dollarValue,
+    required this.portfolioPercentage,
+    required this.priceChange,
+    required this.percentageChange,
+  });
+}
--- lib/features/portfolio/presentation/widgets/my_holding_widget.dart
+++ lib/features/portfolio/presentation/widgets/my_holding_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fintech_app/features/portfolio/data/models/my_holding_model.dart';
 import 'package:fintech_app/features/portfolio/presentation/widgets/holding_item_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,37 +7,54 @@ class MyHoldingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> icons = [
-      'assets/svgs/bitcoin.svg',
-      'assets/svgs/ethereum.svg',
-      'assets/svgs/lc.svg',
+    final List<MyHoldingModel> holdings = [
+      MyHoldingModel(
+        icon: 'assets/svgs/bitcoin.svg',
+        name: 'Bitcoin',
+        symbol: 'BTC',
+        amount: '0.05 BTC',
+        dollarValue: '\$2,262.58',
+        portfolioPercentage: '50%',
+        priceChange: 145.20,
+        percentageChange: 6.85,
+      ),
+      MyHoldingModel(
+        icon: 'assets/svgs/ethereum.svg',
+        name: 'Ethereum',
+        symbol: 'ETH',
+        amount: '1.5 ETH',
+        dollarValue: '\$3,150.75',
+        portfolioPercentage: '30%',
+        priceChange: 56.70,
+        percentageChange: 1.83,
+      ),
+      MyHoldingModel(
+        icon: 'assets/svgs/lc.svg',
+        name: 'Litecoin',
+        symbol: 'LTC',
+        amount: '26.3 LTC',
+        dollarValue: '\$4,949.76',
+        portfolioPercentage: '20%',
+        priceChange: 120.80,
+        percentageChange: 3.07,
+      ),
     ];
-    final List<String> names = ['Bitcoin', 'Ethereum', 'Litecoin'];
-    final List<String> symbols = ['BTC', 'ETH', 'LTC'];
-    final List<String> amounts = ['0.05 BTC', '1.5 ETH', '26.3 LTC'];
-    final List<String> dollarValues = [
-      '\$2,262.58',
-      '\$3,150.75',
-      '\$4,949.76',
-    ];
-    final List<String> portfolioPercentages = ['50%', '30%', '20%'];
-    final List<double> priceChanges = [145.20, 56.70, 120.80];
-    final List<double> percentageChanges = [6.85, 1.83, 3.07];
 
     return ListView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      itemCount: names.length,
+      itemCount: holdings.length,
       itemBuilder: (context, index) {
+        final holding = holdings[index];
         return HoldingItemWidget(
-          icon: icons[index],
-          name: names[index],
-          symbol: symbols[index],
-          amount: amounts[index],
-          dollarValue: dollarValues[index],
-          portfolioPercentage: portfolioPercentages[index],
-          priceChange: priceChanges[index],
-          percentageChange: percentageChanges[index],
+          icon: holding.icon,
+          name: holding.name,
+          symbol: holding.symbol,
+          amount: holding.amount,
+          dollarValue: holding.dollarValue,
+          portfolioPercentage: holding.portfolioPercentage,
+          priceChange: holding.priceChange,
+          percentageChange: holding.percentageChange,
         );
       },
     );
