723d840a1ef164f0c832f84b8efd2e1b5a0fc770
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-12-03 19:29:44 +0200

    - add shimmer and make version of build runner compitable with test

--- lib/features/coin/data/data_source/coins_remote_data_source.g.dart
+++ lib/features/coin/data/data_source/coins_remote_data_source.g.dart
@@ -8,7 +8,7 @@ part of 'coins_remote_data_source.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter,avoid_unused_constructor_parameters,unreachable_from_main
 
 class _CoinsRemoteDataSource implements CoinsRemoteDataSource {
   _CoinsRemoteDataSource(this._dio, {this.baseUrl, this.errorLogger}) {
@@ -42,7 +42,7 @@ class _CoinsRemoteDataSource implements CoinsRemoteDataSource {
     try {
       _value = CoinResponseModel.fromJson(_result.data!);
     } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options,_result);
+      errorLogger?.logError(e, s, _options, _result);
       rethrow;
     }
     return _value;
@@ -76,7 +76,7 @@ class _CoinsRemoteDataSource implements CoinsRemoteDataSource {
     try {
       _value = CoinChartResponseModel.fromJson(_result.data!);
     } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options,_result);
+      errorLogger?.logError(e, s, _options, _result);
       rethrow;
     }
     return _value;
@@ -103,7 +103,7 @@ class _CoinsRemoteDataSource implements CoinsRemoteDataSource {
     try {
       _value = _result.data!.cast<String>();
     } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options,_result);
+      errorLogger?.logError(e, s, _options, _result);
       rethrow;
     }
     return _value;
